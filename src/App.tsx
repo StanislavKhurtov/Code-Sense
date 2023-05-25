1187a333b54b01ac7242d2f2043e5d1f64c26826
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-25 10:39:04 +0300

    feat: add project file

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import Accordeon from "./Components/Accordeon/Accordeon";
+
+
+/*function hello() {
+    debugger
+    alert("Hello ")
+}
+hello();
+*/
+
+//компонента всегда начинается с заглавной буквы
+const App = () => {
+    //пишем логику
+    console.log("App rendering");
+    return (
+        //пишем JSX
+        <div>
+            {/*<PageTitle title={'This is Element'}/>
+            <PageTitle title={'This is My Friends'}/>
+            <Rating value={3}/>*/}
+            <Accordeon titleValue={'Menu'} collapsed={true}/>
+            <Accordeon titleValue={'User'} collapsed={false}/>
+            {/*<Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>*/}
+        </div>
+    );
+}
+type PageTitlePropsType = {
+    title: string,
+}
+const PageTitle = (props: PageTitlePropsType) => {
+    return <h1>{props.title}</h1>
+}
+
+
+export default App;
---
+++ src/Components/Accordeon/Accordeon.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+type AccordeonPropsType = {
+    titleValue: string,
+    collapsed: boolean,
+}
+
+const Accordeon = (props: AccordeonPropsType) => {
+    debugger
+    console.log("Accordeon rendering");
+
+
+    if (props.collapsed === false) {
+        return (
+            <div>
+                <AccordionTitle title={props.titleValue} />
+                <AccordeonBody/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} />
+        </div>
+    )
+
+}
+type AccTitlePropsType = {
+    title: string,
+}
+
+const AccordionTitle = (props: AccTitlePropsType) => {
+    debugger
+    console.log("AccordionTitle rendering");
+    return (
+        <h3>{props.title}</h3>
+    );
+}
+const AccordeonBody = () => {
+    console.log("AccordeonBody rendering");
+    return (<ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>);
+}
+
+export default Accordeon;
+
+
+
