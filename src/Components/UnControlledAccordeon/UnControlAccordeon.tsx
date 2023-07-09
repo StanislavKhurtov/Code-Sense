6c3c036aebd68438dea863af9e991d9555da5c67
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-09 19:33:01 +0300

    feat: lesson #16 useReducer

--- src/Components/UnControlledAccordeon/UnControlAccordeon.tsx
+++ src/Components/UnControlledAccordeon/UnControlAccordeon.tsx
@@ -1,31 +1,35 @@
-import React, {useReducer, useState} from "react";
+import React, {useReducer} from "react";
 
 type UnControlAccordeonPropsType = {
     title: string,
 }
 
-type ActionType={
-    type:string
+type ActionType = {
+    type: string
 }
 
-const reducer = (state: boolean, action:ActionType) => {
-if(action.type==="Toggle-collapsed"){
-    return !state;
-}
-    return state;
+const reducer = (state: boolean, action: ActionType) => {
+    switch (action.type) {
+        case "Toggle-collapsed":
+            return !state;
+        default:
+            return state;
+            //throw new
+    }
+
 }
 
 export const UnControlAccordeon = (props: UnControlAccordeonPropsType) => {
 
-   // let [collapsed, setCollapsed] = useState(false);
-    let [collapsed, dispatch] = useReducer(reducer,false);
+    // let [collapsed, setCollapsed] = useState(false);
+    let [collapsed, dispatch] = useReducer(reducer, false);
 
     console.log(collapsed)
 
     return (
         <div>
             {/*<AccordionTitle title={props.title} onClick={() => setCollapsed(!collapsed)}/>*/}
-            <AccordionTitle title={props.title} onClick={() => dispatch({type:"Toggle-collapsed"})}/>
+            <AccordionTitle title={props.title} onClick={() => dispatch({type: "Toggle-collapsed"})}/>
             {collapsed && <AccordeonOnBody/>}
         </div>
     )
