4ff66b44245db9b78a328f92ede6c1efee987a96
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-11 16:50:52 +0300

    feat : lesson #16 React memo

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -8,9 +8,13 @@ const NewMessageCounter = (props: any) => {
 }
 
 
-const Users = (props: { users: Array<string> }) => {
-    return <div>{props.users.map((u, i) => <div>{u}</div>)}</div>
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("USERS")
+    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
 }
+
+const Users = React.memo(UsersSecret);
+
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Stas', "Fedor", "Eblan"])
