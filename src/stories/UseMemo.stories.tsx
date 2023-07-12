18901be2851ff7afcb72e25d380f326107b82fe5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-12 11:07:22 +0300

    fix : lesson #17 add useMemo newArray

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -26,7 +26,6 @@ export const DifficultCountingExample = () => {
     }, [a])
 
 
-
     for (let i = 1; i <= b; i++) {
         resultB *= i
     }
@@ -44,46 +43,33 @@ export const DifficultCountingExample = () => {
             </div>
         </div>
     );
+};
+
+
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("USERS Sect")
+    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
 }
 
+const Users = React.memo(UsersSecret);
 
-export const HelpForReactMemoExample = () => {
-    const [a, setA] = useState<number>(5);
-    const [b, setB] = useState<number>(5);
 
-    let resultA = 1;
-    let resultB = 1;
+export const HelpsToReactMemo = () => {
 
-    resultA = useMemo(() => {
-        let tempResultA = 1;
-        for (let i = 1; i <= a; i++) {
-            let b = 0
-            while (b < 1000000) {
-                const fakeValue = Math.random();
-                b++;
-            }
-            tempResultA *= i
-        }
-        return tempResultA
-    }, [a])
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Stas', "Fedor", "Eblan","Vasia"])
+
+    const newArray = useMemo(()=> {
+       return  users.filter(el => el.toLowerCase().indexOf("a") > 1)
+    },[])
 
 
 
-    for (let i = 1; i <= b; i++) {
-        resultB *= i
-    }
-
-    return (
-        <div>
-            <input value={a} onChange={(e: ChangeEvent<HTMLInputElement>) => setA(Number(e.currentTarget.value))}/>
-            <input value={b} onChange={(e: ChangeEvent<HTMLInputElement>) => setB(+e.currentTarget.value)}/>
-            <hr/>
-            <div>
-                Result for a : {resultA}
-            </div>
-            <div>
-                Result for b : {resultB}
-            </div>
-        </div>
-    );
+    return <div>
+        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+        {counter}
+        <Users users={newArray}/>
+    </div>
 }
+
