fda81715512889f96fc569d2451e534dd8c9ae84
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-12 10:31:50 +0300

    feat : lesson #17 add use memo

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,41 @@
+import React, {ChangeEvent, useState} from "react";
+
+export default {
+    title: 'useMemo',
+}
+
+
+export const Example1 = () => {
+    const [a, setA] = useState<number>(5);
+    const [b, setB] = useState<number>(5);
+
+    let resultA = 1;
+    let resultB = 1;
+
+    for (let i = 1; i <= a; i++) {
+        let b = 0
+        while (b < 1000000) {
+            const fakeValue = Math.random();
+            b++;
+        }
+        resultA *= i
+    }
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return (
+        <div>
+            <input value={a} onChange={(e: ChangeEvent<HTMLInputElement>) => setA(Number(e.currentTarget.value))}/>
+            <input value={b} onChange={(e: ChangeEvent<HTMLInputElement>) => setB(+e.currentTarget.value)}/>
+            <hr/>
+            <div>
+                Result for a : {resultA}
+            </div>
+            <div>
+                Result for b : {resultB}
+            </div>
+        </div>
+    );
+}
