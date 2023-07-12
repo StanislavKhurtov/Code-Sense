59dd6134aa5ff2a4adbc73e376d934f6c7de1326
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-12 10:51:30 +0300

    feat : lesson #17 add HelpForReactMemoExample

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -5,7 +5,49 @@ export default {
 }
 
 
-export const Example1 = () => {
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(5);
+    const [b, setB] = useState<number>(5);
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            let b = 0
+            while (b < 1000000) {
+                const fakeValue = Math.random();
+                b++;
+            }
+            tempResultA *= i
+        }
+        return tempResultA
+    }, [a])
+
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return (
+        <div>
+            <input value={a} onChange={(e: ChangeEvent<HTMLInputElement>) => setA(Number(e.currentTarget.value))}/>
+            <input value={b} onChange={(e: ChangeEvent<HTMLInputElement>) => setB(+e.currentTarget.value)}/>
+            <hr/>
+            <div>
+                Result for a : {resultA}
+            </div>
+            <div>
+                Result for b : {resultB}
+            </div>
+        </div>
+    );
+}
+
+
+export const HelpForReactMemoExample = () => {
     const [a, setA] = useState<number>(5);
     const [b, setB] = useState<number>(5);
 
