995bae84923cd90556b2b328f5e57c8b459c3e0c
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-21 14:43:24 +0300

    feat : lesson #17 add useMemo Accordeon

--- src/Components/Accordeon/Accordeon.stories.tsx
+++ src/Components/Accordeon/Accordeon.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import {action} from "@storybook/addon-actions";
 import {Accordeon} from "./Accordeon";
 
@@ -9,23 +9,55 @@ export default {
 }
 
 
-export const CollapsedMode = () => <Accordeon titleValue={"Menu"} collapsed={true}
-                                              onChange={action('accordeon mode change fired')} items={[]}
-                                              onClick={action('accordeon mode change fired')}/>;
-export const UnCollapsedMode = () => <Accordeon titleValue={"Users"} collapsed={false}
-                                                onChange={action('accordeon mode change fired')}
-                                                items={[{title: "Stas", value: 1}, {
-                                                    title: "Egor",
-                                                    value: 2
-                                                }, {title: "Sofia", value: 3}]}
-                                                onClick={action('accordeon mode change fired')}/>;
+export const CollapsedMode = () => <Accordeon
+    titleValue={"Menu"}
+    collapsed={true}
+    onChange={action('accordeon mode change fired')}
+    items={[]}
+    onClick={action('accordeon mode change fired')}
+/>;
+
+
+export const UnCollapsedMode = () => <Accordeon
+    titleValue={"Users"}
+    collapsed={false}
+
+    onChange={action('accordeon mode change fired')}
+    items={[{title: "Stas", value: 1}, {
+        title: "Egor",
+        value: 2
+    }, {title: "Sofia", value: 3}]}
+    onClick={action('accordeon mode change fired')}
+/>;
 
 
 export const ModeChanging = () => {
     const [value, setValue] = useState<boolean>(true)
-    return <Accordeon titleValue={"Users"} collapsed={value} onChange={() => setValue(!value)}
-                      items={[{title: "Stas", value: 1}, {title: "Egor", value: 2}, {title: "Sofia", value: 3}]}
-                      onClick={(id) => {
-                          alert(`user c number ${id} by happy`)
-                      }}/>;
+
+    const onChangeHandler = () => setValue(!value);
+    const onClickHandler = (id:string) => {
+        alert(`user c number ${id} by happy`)
+    }
+
+    const items = useMemo(
+        () => [
+            { title: "Stas", value: 1 },
+            { title: "Egor", value: 2 },
+            { title: "Sofia", value: 3 },
+        ],
+        []
+    );
+
+    return <Accordeon
+        titleValue={"Users"}
+        collapsed={value}
+        onChange={onChangeHandler}
+        items={items}
+        onClick={onClickHandler}
+    />;
 }
+
+
+/*
+Чтобы оптимизировать производительность рендеринга компонента ModeChanging, нужно использовать хук useMemo для запоминания реквизита items, который представляет собой массив объектов, и предотвращения повторного рендеринга.
+*/
--- src/Components/Accordeon/Accordeon.tsx
+++ src/Components/Accordeon/Accordeon.tsx
@@ -1,56 +1,61 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 type ItemType = {
-    title: string
-    value: any
-}
+    title: string;
+    value: any;
+};
 
 type AccordeonPropsType = {
-    titleValue: string
-    collapsed: boolean
-    onChange: () => void
-    items: ItemType[]
-    onClick: (value: any) => void
-}
+    titleValue: string;
+    collapsed: boolean;
+    onChange: () => void;
+    items: ItemType[];
+    onClick: (value: any) => void;
+};
 
 export const Accordeon = (props: AccordeonPropsType) => {
 
+    const accordeonBodyJSX = useMemo(() => {
+        return !props.collapsed && (
+            <AccordeonBody items={props.items} onClick={props.onClick} />
+        );
+    }, [props.collapsed, props.items, props.onClick]);
+
     return (
         <div>
-            <AccordionTitle title={props.titleValue} onChange={props.onChange}/>
-            {!props.collapsed && <AccordeonBody items={props.items} onClick={props.onClick}/>}
+            <AccordionTitle title={props.titleValue} onChange={props.onChange} />
+            {accordeonBodyJSX}
         </div>
-    )
-}
+    );
+};
 
 type AccTitlePropsType = {
-    title: string,
-    onChange: () => void
-}
+    title: string;
+    onChange: () => void;
+};
 
 const AccordionTitle = (props: AccTitlePropsType) => {
-    debugger
-    return (
-        <h3 onClick={(e) => props.onChange()}>{props.title}</h3>
-    );
-}
+    return <h3 onClick={(e) => props.onChange()}>{props.title}</h3>;
+};
 
 type AccordeonBodyPropsType = {
-    items: ItemType[]
-    onClick: (value: any) => void
-}
+    items: ItemType[];
+    onClick: (value: any) => void;
+};
 
 const AccordeonBody = (props: AccordeonBodyPropsType) => {
-
     return (
         <ul>
-            {props.items.map((el, index) => <li onClick={() => {
-                props.onClick(el.value)
-            }} key={index}>{el.title}</li>)}
-        </ul>);
-}
-
-
-
-
-
+            {props.items.map((el, index) => (
+                <li
+                    onClick={() => {
+                        props.onClick(el.value);
+                    }}
+                    key={index}
+                >
+                    {el.title}
+                </li>
+            ))}
+        </ul>
+    );
+};
