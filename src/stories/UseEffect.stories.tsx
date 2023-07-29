02aa111a1ab90ac8020ce832c5dd7443bfe3b8ba
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-29 12:37:11 +0300

    feat: lesson #21 useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -7,18 +7,31 @@ export default {
 
 export const SimpleExample = () => {
 
-
+    const [fake, setFake] = useState<number>(1)
     const [counter, setCounter] = useState<number>(1)
+
     console.log('SimpleExample')
 
-    useEffect(()=> {
-        console.log('useEffect')
+    useEffect(() => {
+        console.log('useEffect every render')
+        document.title = counter.toString()
     })
+
+    useEffect(() => {
+        console.log('useEffect only first render')
+        document.title = counter.toString()
+    },[])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+        document.title = counter.toString()
+    },[counter])
 
     return <div>
         Hello, {counter}
         <button onClick={() => setCounter(counter + 1)}>+</button>
-
+        Hello, {fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
     </div>
 }
 
